3eca639a712fb9260d104beb71d101b24fcdb798
chandra-iragavarapu <138805710+chandra-iragavarapu@users.noreply.github.com>
2024-12-04 12:41:37 +0530

    Initial code setup

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
+})
+export class HomeComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+
+}
